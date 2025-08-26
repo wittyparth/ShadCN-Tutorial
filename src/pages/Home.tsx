a62493d8340758fb63dfddf82dca3708142442e2
Partha Saradhi <parthasaradhimunakala@gmail.com>
2025-08-26 17:37:37 +0530

    made the main layout mobile responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,11 +8,11 @@ const Home = () => {
   return (
     <AppLayout>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
-        <div className="p-4 rounded-md col-span-2 bg-primary-foreground"><ChartBarGraph/></div>
+        <div className="p-4 rounded-md lg:col-span-2 bg-primary-foreground"><ChartBarGraph/></div>
         <div className="p-4 rounded-md bg-primary-foreground"><CardList title="Popular Content"/></div>
         <div className="p-4 rounded-md bg-primary-foreground"><ChartRadarChart/></div>
         <div className="p-4 rounded-md bg-primary-foreground">Test</div>
-        <div className="p-4 rounded-md col-span-2 bg-primary-foreground"><ChartAreaChart/></div>
+        <div className="p-4 rounded-md lg:col-span-2 bg-primary-foreground"><ChartAreaChart/></div>
         <div className="p-4 rounded-md bg-primary-foreground"><CardList title="latestTransactions"/></div>
       </div>
     </AppLayout>
